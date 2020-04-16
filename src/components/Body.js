91e8ae7456e6e1f8693fe319741286e6e09de23c
Luke <lmschless@gmail.com>
2020-04-15 21:57:58 -0700

    working on refactoring Header.js to use classes and state.

--- src/components/Body.js
+++ src/components/Body.js
@@ -10,21 +10,24 @@ function Body() {
 	return (
 		<div style={bodyStyles}>
 			<div>
-				React was originally developed by Facebook to manage the dynamic, complex needs of a large social media app.
-				After observing Facebook's success, many other social media applications have also begun relying on React.
-				Create a site that looks like the mock-up depicted below using only React components. Take care in determining
+				React was originally developed by Facebook to manage the dynamic, complex needs of
+				a large social media app. After observing Facebook's success, many other social
+				media applications have also begun relying on React. Create a site that looks like
+				the mock-up depicted below using only React components. Take care in determining
 				which portions warrant their own components: social-media-wireframe
 			</div>
 			<div>
-				React was originally developed by Facebook to manage the dynamic, complex needs of a large social media app.
-				After observing Facebook's success, many other social media applications have also begun relying on React.
-				Create a site that looks like the mock-up depicted below using only React components. Take care in determining
+				React was originally developed by Facebook to manage the dynamic, complex needs of
+				a large social media app. After observing Facebook's success, many other social
+				media applications have also begun relying on React. Create a site that looks like
+				the mock-up depicted below using only React components. Take care in determining
 				which portions warrant their own components: social-media-wireframe
 			</div>
 			<div>
-				React was originally developed by Facebook to manage the dynamic, complex needs of a large social media app.
-				After observing Facebook's success, many other social media applications have also begun relying on React.
-				Create a site that looks like the mock-up depicted below using only React components. Take care in determining
+				React was originally developed by Facebook to manage the dynamic, complex needs of
+				a large social media app. After observing Facebook's success, many other social
+				media applications have also begun relying on React. Create a site that looks like
+				the mock-up depicted below using only React components. Take care in determining
 				which portions warrant their own components: social-media-wireframe
 			</div>
 		</div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,74 @@
-import React from 'react';
-import Nav from './Nav';
-import Search from './Search';
-import Tweet from './Tweet';
+// import React from 'react';
+// import Nav from './Nav';
+// import Search from './Search';
+// import Tweet from './Tweet';
 
-function Header() {
-	const headerStyles = {
-		height: '15vh',
-		borderBottom: '.15em solid gray'
+// // Original function component, to be replaced with
+// // a class component.
+
+// function Header() {
+// 	const headerStyles = {
+// 		height: '15vh',
+// 		borderBottom: '.15em solid gray'
+// 	};
+// 	return (
+// 		<React.Fragment>
+// 			<header>
+// 				<div style={headerStyles}>
+// 					<Nav />
+// 					<Tweet />
+// 					<Search />
+// 				</div>
+// 			</header>
+// 		</React.Fragment>
+// 	);
+// }
+
+// export default Header;
+
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+	state = {
+		count: 0
 	};
-	return (
-		<React.Fragment>
-			<header>
-				<div style={headerStyles}>
-					<Nav />
-					<Tweet />
+	render() {
+		const headerStyles = {
+			height: '15vh',
+			borderBottom: '.15em solid gray'
+		};
+		return (
+			<React.Fragment>
+				<header style={headerStyles}>{this.tweet()}</header>
+			</React.Fragment>
+		);
+	}
 
-					<Search />
-				</div>
-			</header>
-		</React.Fragment>
-	);
+	tweet() {
+		const tweetStyles = {
+			width: '7em',
+			height: '3em',
+			float: 'right',
+			marginTop: '2.5em',
+			marginRight: '1em',
+			borderRadius: '2em',
+			border: 'solid',
+			backgroundColor: 'white',
+			borderColor: '#478fd5'
+		};
+		return (
+			<React.Fragment>
+				<p>{this.state.count}</p>
+				<button style={tweetStyles} onClick={() => this.handleClick()}>
+					Tweet
+				</button>
+			</React.Fragment>
+		);
+	}
+
+	handleClick() {
+		return this.setState({
+			count: (this.state.count += 1)
+		});
+	}
 }
-
-export default Header;
